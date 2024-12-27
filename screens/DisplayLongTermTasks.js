c6b2d965b5dde00edadb26ed90111cafe10e743e
Umesh_Yapa <umeshmadhusankha19@gmail.com>
2024-12-28 00:34:07 +0530

    minor fix

--- screens/DisplayLongTermTasks.js
+++ screens/DisplayLongTermTasks.js
@@ -1,4 +1,4 @@
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity } from 'react-native'
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { SafeAreaView } from 'react-native-safe-area-context';
--- screens/DisplayTasksScreen.js
+++ screens/DisplayTasksScreen.js
@@ -1,4 +1,4 @@
-import { ScrollView, Text, StyleSheet, Button, View } from 'react-native'
+import { ScrollView, Text, StyleSheet, TouchableOpacity, View } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { useSharedState } from '../hooks/useSharedState';
