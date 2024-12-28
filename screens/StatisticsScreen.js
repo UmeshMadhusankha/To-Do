2b939b704adff93ae4a2625bd3809b7b1212aa32
Umesh_Yapa <umeshmadhusankha19@gmail.com>
2024-12-28 22:04:29 +0530

    pie sizes changed

--- screens/StatisticsScreen.js
+++ screens/StatisticsScreen.js
@@ -7,8 +7,8 @@ import { useSelector } from 'react-redux';
 const StatisticsScreen = () => {
 
   // variables for pie chart
-  const widthAndHeight = 250;
-  const sliceColor = ['rgba(0,255,0,0.8)', 'rgba(255,0,0,0.2)']
+  const widthAndHeight = 240;
+  const sliceColor = ['rgba(0, 255, 255, 0.5)', 'rgba(0, 255, 0, 0.5)','rgba(255, 0, 0, 0.5)','gray']
 
   const tasks = useSelector((state) => state.tasks);
 
@@ -20,15 +20,26 @@ const StatisticsScreen = () => {
   
   let totalTodayTasks = 0;
   let completedTodayTasks = 0;
+  let onGoingTodayTasks = 0;
+  let failedTodayTasks = 0;
+  let fallbackValue1 = 0;
 
   todayTasks.forEach(element => {
     totalTodayTasks++;
-    if (element.value.status == 2) {
+    if (element.value.status == 1) {
+      onGoingTodayTasks++;
+    }
+    else if (element.value.status == 2) {
       completedTodayTasks++;
+    }
+    else {
+      failedTodayTasks++;
     }
   });
 
-  let series1 = [completedTodayTasks/totalTodayTasks, 1 - (completedTodayTasks/totalTodayTasks)];
+  fallbackValue1 = totalTodayTasks == 0 ? 1 : 0;
+  
+  let series1 = [completedTodayTasks, onGoingTodayTasks, failedTodayTasks, fallbackValue1];
 
 // calculating weeks' success rate
   const previousWeek = [];
@@ -49,30 +60,52 @@ const StatisticsScreen = () => {
 
   let totalWeekTasks = 0;
   let completedWeekTasks = 0;
+  let onGoingWeekTasks = 0;
+  let failedWeekTasks = 0;
+  let fallbackValue2 = 0;
 
   prevWeekTasks.forEach((element) => {
     totalWeekTasks++;
-    if (element.value.status == 2) {
+    if (element.value.status == 1) {
+      onGoingWeekTasks++;
+    }
+    else if (element.value.status == 2) {
       completedWeekTasks++;
+    }
+    else {
+      failedWeekTasks++;
     }
   })
 
-  let series2 = [(completedWeekTasks/totalWeekTasks), 1 - (completedWeekTasks/totalWeekTasks)];
+  fallbackValue2 = totalWeekTasks == 0 ? 1 : 0;
+
+  let series2 = [completedWeekTasks,onGoingWeekTasks,failedWeekTasks,fallbackValue2];
 
 // calculating long term works' success rate
   const longTermTasks = useSelector((state) => state.longTermTasks);
 
   let totalLongTermTasks = 0;
   let completedLongTermTasks = 0;
+  let onGoingLongTermTasks = 0;
+  let failedLongTermTasks = 0;
+  let fallbackValue3 = 0;
 
   longTermTasks.forEach((element) => {
     totalLongTermTasks++;
-    if (element.value.status == 2) {
+    if (element.value.status == 1) {
+      onGoingLongTermTasks++;
+    }
+    else if (element.value.status == 2) {
       completedLongTermTasks++;
+    }
+    else {
+      failedLongTermTasks++;
     }
   })
 
-  let series3 = [(completedLongTermTasks/totalLongTermTasks), 1 - (completedLongTermTasks/totalLongTermTasks)];
+  fallbackValue3 = totalLongTermTasks == 0 ? 1 : 0;
+
+  let series3 = [completedLongTermTasks,onGoingLongTermTasks,failedLongTermTasks,fallbackValue3];
 
 
   return (
@@ -88,7 +121,7 @@ const StatisticsScreen = () => {
             style={styles.pie1}
           />
           <View style={styles.stat1}>
-            <Text style={styles.stat_text1}>{(completedTodayTasks/totalTodayTasks) * 100}</Text>
+            <Text style={styles.stat_text1}>{totalTodayTasks == 0 ? 0 : Math.trunc((completedTodayTasks/totalTodayTasks) * 100)}</Text>
             <Text style={styles.stat_text2}>percent</Text>
           </View>
         </View>
@@ -103,7 +136,7 @@ const StatisticsScreen = () => {
             style={styles.pie1}
           />
           <View style={styles.stat1}>
-            <Text style={styles.stat_text1}>{(completedWeekTasks/totalWeekTasks) * 100}</Text>
+            <Text style={styles.stat_text1}>{totalWeekTasks == 0 ? 0 : Math.trunc((completedWeekTasks/totalWeekTasks) * 100)}</Text>
             <Text style={styles.stat_text2}>percent</Text>
           </View>
         </View>
@@ -118,7 +151,7 @@ const StatisticsScreen = () => {
             style={styles.pie1}
           />
           <View style={styles.stat1}>
-            <Text style={styles.stat_text1}>{(completedLongTermTasks/totalLongTermTasks) * 100}</Text>
+            <Text style={styles.stat_text1}>{totalLongTermTasks == 0 ? 0 : Math.trunc((completedLongTermTasks/totalLongTermTasks) * 100)}</Text>
             <Text style={styles.stat_text2}>percent</Text>
           </View>
         </View>
@@ -140,7 +173,7 @@ const styles = {
   },
   pie_topics: {
     textAlign: 'center',
-    fontSize: 20,
+    fontSize: 21,
     fontWeight: 500,
     padding: 10
   },
@@ -148,9 +181,9 @@ const styles = {
     margin: 10
   },
   stat1: {
-    backgroundColor: '#ddd',
-    width: '185',
-    height: '185',
+    backgroundColor: '#eee',
+    width: '180',
+    height: '180',
     position: 'absolute',
     top: 100,
     justifyContent: 'center',
@@ -160,12 +193,12 @@ const styles = {
     textAlign: 'center',
     fontSize: 50,
     fontWeight: 800,
-    color: 'green'
+    color: '#02ba39'
   },
   stat_text2: {
     textAlign: 'center',
     fontSize: 15,
     fontWeight: 500,
-    color: 'green'
+    color: '#02ba39'
   }
 }
